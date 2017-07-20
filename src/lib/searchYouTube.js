ea8e08dcfc73332ccd881656d958a8f934bb7220
Hack Reactor Student <student@hackreactor.com>
2017-07-20 11:11:21 -0700

    Implement debounce on search

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,19 +1,21 @@
-var searchYouTube = ( {key = YOUTUBE_API_KEY, query, max = 5 }, callback) => {
-  setTimeout($.get.bind(null, ('https://www.googleapis.com/youtube/v3/search', {
-    part: 'snippet',
-    key: key,
-    q: query, 
-    maxResults: max,
-    type: 'video',
-    videoEmbeddable: 'true',
-  }).done((data) => {
-    if (callback) {
-      callback(data.items);
-    }
-  }).fail((err) => {
-    console.log('GET failed', err);
-  }), 500)
-  );
+var searchYouTube = ( {key = YOUTUBE_API_KEY, query = '', max = 5 }, callback) => { 
+  var debouncedFunc = _.debounce( () => {
+    $.get('https://www.googleapis.com/youtube/v3/search', {
+      part: 'snippet',
+      key: key,
+      q: query, 
+      maxResults: max,
+      type: 'video',
+      videoEmbeddable: 'true',
+    }).done((data) => {
+      if (callback) {
+        callback(data.items);
+      }
+    }).fail((err) => {
+      console.log('GET failed', err);
+    });
+  }, 500, true);
+  debouncedFunc();
 };
 
 window.searchYouTube = searchYouTube;
